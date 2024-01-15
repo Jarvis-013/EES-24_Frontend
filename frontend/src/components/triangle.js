d65e04c23db4e8d72936897e8a96062b6bacf87b
Jarvis-013 <124374386+Jarvis-013@users.noreply.github.com>
2024-01-16 01:02:47 +0530

    Update triangle.js

--- frontend/src/components/triangle.js
+++ frontend/src/components/triangle.js
@@ -1,32 +1,284 @@
-import React from 'react';
+.SignUp {
+  /* font-size: 1rem;  */
 
-const Triangle = () => {
-  const triangleStyle = {
-    width: '0',
-    height: '0',
-    borderLeft: '50px solid transparent',
-    borderRight: '50px solid transparent',
-    borderBottom: '90px solid #fff', // Height of the equilateral triangle
-  };
+  color: #000;
+  text-align: center;
+  font-family: Goldman;
+  font-size: 300%;
+  margin-top: 8%;
+  padding: 2%;
+  font-style: normal;
+  font-weight: 200;
+  line-height: normal;
+  letter-spacing: 5.775px;
+  margin-left: 1%;
+}
+.amaan{width: 40%;}
+.ees-logo-mob{
+  display: none;
+}
+.ees-logo-tab{
+  display: none;
+}
+@media only screen and (max-width: 1100px) and (min-width:900px) {
+  .ees-logo-tab{
+    display: block;
+  }
+  .svg-tab{
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    width: 120%;
+    height: 200%;
+    margin-top: 10%;
+    transform: scale(1.2);
+    margin-bottom:2% ;
+    z-index: 1;
+  }
+  
+  .SignUp-card{
+    width: 50%;
+    height: 100%;
+  }
 
-  return (
-    <div style={{
-      position:'relative',
-        width: 'auto',
-        height: 'auto',
-        zIndex:'-1',
-       
-        borderLeft: '3rem solid transparent',
-        borderRight: '3rem solid transparent',
-        borderBottom: '5rem solid #fff',
-        transform:'rotate(90deg)' ,
-        position:'absolute',
-        top:'-2%',
-        left:'-1%',
-    }}>
-      {/* Your content goes here */}
-    </div>
-  );
-};
+  .SignUp-card-blackDiv{
+    width: 80%;
+  }
 
-export default Triangle;
+  .amaan{
+    width: 100%;
+  }
+}
+
+
+
+
+
+.SignUp-card-whiteDiv {
+  background-image: url("/public/Vector 91.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.SignUp-card-blackDiv {
+  background-image: url("../../public/Vector 92.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position-x: center;
+  background-size: 100%;
+}
+
+.EES-logo {
+  background-image: url("../../public/ees_logo_FCFBFC 1.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.SignUp-card-heading {
+  color: #000;
+  text-align: center;
+  font-family: Goldman;
+  font-size: 77px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 5.775px;
+}
+
+.SignUp-card-blackDiv-whiteEESpattern {
+  background-image: url("../../public/Vector 93.svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.SignUp-card-blackDiv-eesdesign {
+  background-image: url("../../public/EES-design-signup page .svg");
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+/* styles.css */
+
+.white-placeholder::placeholder {
+  color: white;
+  /* Add any other styles you want for the placeholder text */
+}
+
+.signup-button-blackDiv {
+  background-image: url("../../public/Vector 95.svg");
+  background-image: cover;
+  background-repeat: no-repeat;
+  color: #000;
+  text-align: center;
+  font-family: Goldman;
+
+  font-size: 1.75rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 5.775px;
+}
+
+.signup-button-blackDivAlreadyHaveAnAccount {
+  color: #fff;
+
+  text-align: center;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Goldman;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+}
+
+.signupButton-text {
+  color: black;
+  text-align: center;
+  font-family: Goldman;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+}
+
+.signup-button-blackDivAlreadyHaveAnAccountButton {
+  background-image: url("../../public/Vector 94.svg");
+  background-repeat: no-repeat;
+  background-size: contain;
+  color: black;
+  text-align: center;
+  font-family: Goldman;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 1.2px;
+}
+
+.SignUpPage {
+  position: relative;
+}
+
+.SignUpPage::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-image: url("/public/image 36.svg");
+  background-size: cover;
+  filter: blur(8px);
+  opacity: 1; /* Adjust the opacity for transparency */
+  z-index: -1;
+}
+.svg-sign{
+  display: none;
+}
+button:hover {
+  cursor: pointer;
+}
+
+.button-container {
+  text-align: center;
+  font-family: Goldman;
+  height: 40%;
+  width: 100%;
+  opacity: 0;
+  display: none; /* Hide by default */
+}
+
+.placeholder {
+  color: white;
+  opacity: 1; /* Firefox */
+}
+
+@media only screen and (max-width: 1100px) {
+  .SignUp-card-whiteDiv {
+    background-image: unset;
+    background: white;
+    height: 20%;
+    margin-bottom: 20%;
+  }
+
+  
+  
+  .none {
+    display: none;
+  }
+
+  .SignUp-card-blackDiv {
+    background-image: unset;
+    background: rgba(0, 0, 0, 0.7);
+    height: 80%; padding: 1%;
+  }
+  
+  .amaan {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    height: auto;
+  }
+  .ees-logo-mob{
+    display: block;
+  }
+  input {
+    margin-bottom: 8%;
+  }
+  .nomargin {
+    margin-bottom: 0;
+  }
+
+  .zigzagPattern {
+    display: none;
+  }
+
+  .ButtonContainerRightMostDiv {
+    display: none;
+  }
+
+  .button-container {
+    display: block; /* Make visible when screen size is 900px or below */
+    opacity: 1; /* You can adjust the opacity if needed */
+    height: 40%;
+    width: 100%;
+  }
+  .laptopDesign {
+    display: none;
+  }
+
+  .SignUp-card-whiteDiv{
+    display: none;
+  }
+
+  .svg-sign{
+    display:block ;
+    padding:none;
+    margin-left:-30%;
+    margin-top:-20%;
+  }
+  .SignUp{
+    display: none;
+  }
+.SignUp-card-blackDiv{
+  margin-top:-55%;
+  margin-left: -50px;
+  padding:3%;
+  width: 120%;;
+}
+
+.SignUp-card-whiteDiv{
+  width: 100%;
+}
+
+.svg-tab{
+  width: 119%;
+  margin-left: -1%;
+}
+
+.ees-logo-mob{
+  display: block;
+}
+}
